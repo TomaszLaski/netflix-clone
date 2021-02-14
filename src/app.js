0e366e5e24026457bdf2127135a19c6481da09c5
TomaszLaski <tomasz.laski0001@gmail.com>
2021-02-14 20:11:48 +0100

    add Context and State to Accordion

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import Home from './pages/home'
+
 export function App() {
   return (
     <Router>
