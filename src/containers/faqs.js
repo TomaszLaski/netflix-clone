d112c33650d0c49a5efcc1b5ed5ee62f41342ad4
TomaszLaski <tomasz.laski0001@gmail.com>
2021-02-14 19:41:44 +0100

    add accordion styles

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -4,7 +4,15 @@ import faqsData from '../fixtures/faqs.json'
 
 export function FaqsContainer() {
     return (
-    <Accordion>
-        <Accordion.Title>Fequently Asked Questions</Accordion.Title>
-    </Accordion>
-    )}
+        <Accordion>
+            <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+            <Accordion.Frame>
+                {faqsData.map((item) => (
+                    <Accordion.Item key={item.id}>
+                        <Accordion.Header>{item.header}</Accordion.Header>
+                        <Accordion.Body>{item.body}</Accordion.Body>
+                    </Accordion.Item>
+                ))}
+            </Accordion.Frame>
+        </Accordion>);
+}
