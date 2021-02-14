4a7e59016b7c8a59fb319dd953ee2dd19da5cc73
TomaszLaski <tomasz.laski0001@gmail.com>
2021-02-14 18:17:40 +0100

    Building the Accordion Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-import { findRenderedComponentWithType } from 'react-dom/test-utils';
-
+export { default as Accordion } from './accordion'
 export { default as Jumbotron } from './jumbotron';
 export { default as Footer } from './footer';
---
+++ src/containers/faqs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Accordion } from '../components';
+import faqsData from '../fixtures/faqs.json'
+
+export function FaqsContainer() {
+    return (
+    <Accordion>
+        <Accordion.Title>Fequently Asked Questions</Accordion.Title>
+    </Accordion>
+    )}
