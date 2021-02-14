f98cbb3c50e80c7ff0697e31fd255dece4409f36
TomaszLaski <tomasz.laski0001@gmail.com>
2021-02-14 23:16:36 +0100

    styling Feature Component

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,17 +9,17 @@ export default function Home() {
     return (
         <>
             <HeaderContainer>
-                <OptForm>
-                    <Feature>
-                        <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
-                        <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+                <Feature>
+                    <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+                    <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+                    <OptForm>    
                         <OptForm.Input placeholder="Email Address" />
                         <OptForm.Button>Try it now</OptForm.Button>
                         <OptForm.Break />
                         <OptForm.Text>Ready to watch? Enter your email to create or restart
                         your membership.</OptForm.Text>
-                    </Feature>
-                </OptForm>
+                    </OptForm>
+                </Feature>
             </HeaderContainer>            
             <JumbotronContainer />
             <FaqsContainer />
