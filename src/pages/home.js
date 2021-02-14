449dc3ddecb7251848c4b33e80d68013944c4f38
TomaszLaski <tomasz.laski0001@gmail.com>
2021-02-14 22:33:03 +0100

    build up Header Component

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,10 +2,14 @@ import React from 'react'
 import { FaqsContainer } from '../containers/faqs'
 import { JumbotronContainer } from '../containers/jumbotron'
 import { FooterContainer } from '../containers/footer'
+import { HeaderContainer} from '../containers/header'
 
 export default function Home(){
     return(
         <>        
+        <HeaderContainer>
+            <p>hello</p>
+        </HeaderContainer>
         <JumbotronContainer/>
         <FaqsContainer />
         <FooterContainer/>
