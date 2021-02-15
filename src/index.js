2cd2a26088d500c57b3199d2c3c9a705fa62068e
TomaszLaski <tomasz.laski0001@gmail.com>
2021-02-15 16:18:54 +0100

    connecting app to Firebase

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import { App } from './app';
 import { FirebaseContext } from './context/firebase';
 
 const config = {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-}
+    apiKey: "AIzaSyBTATt4ckjNes0tvpPoSeLs8dBaaHoV2Ac",
+    authDomain: "netflix-clone-74b49.firebaseapp.com",
+    databaseURL: "https://netflix-clone-74b49-default-rtdb.firebaseio.com",
+    projectId: "netflix-clone-74b49",
+    storageBucket: "netflix-clone-74b49.appspot.com",
+    messagingSenderId: "464693240837",
+    appId: "1:464693240837:web:6569b63e7a81cdbe12686c"
+  }
 
 const firebase = window.firebase.initializeApp(config);
 
