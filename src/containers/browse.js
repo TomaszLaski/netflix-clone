b81fcc8c6ebd03ccb1bcb97198f5c7a04109cb71
TomaszLaski <tomasz.laski0001@gmail.com>
2021-02-17 17:23:13 +0100

    add a Dropdown (Profiles) to Header Component

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -37,6 +37,19 @@ export function BrowseContainer() {
                     </Header.Group>
                     <Header.Group>
                         <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                        <Header.Profile>
+                            <Header.Picture src={user.photoURL} />
+                            <Header.Dropdown>
+                                <Header.Group>
+                                    <Header.Picture src={user.photoURL} />
+                                    <Header.Link>{user.displayName}</Header.Link>
+                                </Header.Group>
+                                <Header.Group>
+                                    <Header.Link onClick={() => firebase.auth().signOut()}>
+                                        Sign out</Header.Link>
+                                </Header.Group>
+                            </Header.Dropdown>
+                        </Header.Profile>
                     </Header.Group>
                 </Header.Frame>
                 
