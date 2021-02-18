dc61138b0ec76324ecc1c1bf3571f611c914b2ee
TomaszLaski <tomasz.laski0001@gmail.com>
2021-02-18 19:13:17 +0100

    add Card Component Skeleton

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Loading, Header } from '../components';
+import { Card, Loading, Header } from '../components';
 import * as ROUTES from '../constants/routes';
 import { FirebaseContext } from '../context/firebase';
 import { SelectProfileContainer } from './profiles';
 import { FooterContainer } from './footer';
 
-export function BrowseContainer() {
+export function BrowseContainer({ slides }) {
     const [category, setCategory] = useState('series');
     const [profile, setProfile] = useState({});
     const [loading, setLoading] = useState(true);
     const [searchTerm, setSearchTerm] = useState('');
+    const [slideRows, setSlideRows] = useState([]);
     
     const { firebase } = useContext(FirebaseContext);
     
@@ -17,16 +18,21 @@ export function BrowseContainer() {
         displayName: "Tomasz",
         photoURL: "1"
     };
-
+    
     useEffect(() => {
         setTimeout(() => {
             setLoading(false)
         }, 3000);
     }, [user])
     
+    useEffect(() => {
+        setSlideRows(slides[category]);
+    }, [slides, category]);
+    
     return profile.displayName ? (
         <>
-        {loading? <Loading src={user.photoURL} /> : <Loading.ReleaseBody/>}    
+        {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+        
             <Header src="joker1" dontShowOnSmallViewPort>
                 <Header.Frame>
                     <Header.Group>
@@ -70,6 +76,25 @@ export function BrowseContainer() {
                     <Header.PlayButton>Play</Header.PlayButton>
                 </Header.Feature>
             </Header>
+            
+            <Card.Group>
+                {slideRows.map((slideItem) => (
+                    <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+                        <Card.Title>{slideItem.title}</Card.Title>
+                        <Card.Entities>
+                            {slideItem.data.map((item) => (
+                                <Card.Item key={item.docId} item={item}>
+                                    <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                                    <Card.Meta>
+                                        <Card.SubTitle>{item.title}</Card.SubTitle>
+                                        <Card.Text>{item.description}</Card.Text>
+                                    </Card.Meta>
+                                </Card.Item>
+                            ))}
+                        </Card.Entities>
+                    </Card>
+                ))}
+            </Card.Group>
             <FooterContainer />
         </>)
         : (<SelectProfileContainer user={user} setProfile={setProfile} />);
